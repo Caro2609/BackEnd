1e3f68c1cedd4ca0684bc8a4e64c3341e9597b22
Caro <ccaro2609@gmail.com>
2022-10-29 12:52:58 -0300

    primer commit

---
+++ src/main/java/com/portfolio/cmr/Service/ImpPersonaService.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.cmr.Service;
+
+import com.portfolio.cmr.Entity.Persona;
+import com.portfolio.cmr.Interface.IPersonaService;
+import com.portfolio.cmr.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+    
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
---
+++ src/test/java/com/portfolio/cmr/CmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.cmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
