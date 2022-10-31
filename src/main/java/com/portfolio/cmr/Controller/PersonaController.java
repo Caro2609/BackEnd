9a891e4a27ed0bebc2cbe4752ee39f682592e59c
Caro <ccaro2609@gmail.com>
2022-10-30 21:15:34 -0300

    primer commit en heroku

--- src/main/java/com/portfolio/cmr/Controller/PersonaController.java
+++ src/main/java/com/portfolio/cmr/Controller/PersonaController.java
@@ -8,7 +8,7 @@ import com.portfolio.cmr.Entity.Persona;
 import com.portfolio.cmr.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 //@CrossOrigin(origins = "https://cmrfrontend.web.app")
 public class PersonaController {
      @Autowired IPersonaService ipersonaService;
