92a904d775d0555b64d0de9896946451caa48dcc
Caro <ccaro2609@gmail.com>
2022-10-31 19:16:35 -0300

    cambios img

--- src/main/java/com/portfolio/cmr/Dto/dtoPersona.java
+++ src/main/java/com/portfolio/cmr/Dto/dtoPersona.java
@@ -2,41 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.portfolio.cmr.Entity;
+package com.portfolio.cmr.Dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
 
-@Entity
-public class Persona {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    
-    @NotNull
-    @Size(min = 4, max = 50, message = "No cumple con la longitud")
+
+public class dtoPersona {
+   
+    @NotBlank
     private String nombre;
-    
-    @NotNull
-    @Size(min = 4, max = 50, message = "No cumple con la longitud")
+    @NotBlank
     private String apellido;
-    
-    @NotNull
+    @NotBlank
     private String descripcion;
-    
-    private String img;
+    @NotBlank
+    private String img; 
     
     
     //Constructor
 
-    public Persona() {
+    public dtoPersona() {
     }
 
-    public Persona(String nombre, String apellido, String descripcion, String img) {
+    public dtoPersona(String nombre, String apellido, String descripcion, String img) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.descripcion = descripcion;
@@ -45,14 +33,6 @@ public class Persona {
     
     
     //Getters y Setters
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getNombre() {
         return nombre;
@@ -85,4 +65,5 @@ public class Persona {
     public void setImg(String img) {
         this.img = img;
     }
+    
 }
